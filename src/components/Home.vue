a782ee5404a71727d377cd568a11b44da26178ea
hi518 <2362391092@qq.com>
2020-11-19 01:18:33 +0800

    完成了登录页面

---
+++ src/components/Home.vue
@@ -0,0 +1,28 @@
+<!-- 主页部分 -->
+<template>
+  <div>
+    <el-button @click="logout" type="info">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  },
+  methods: {
+    logout() {
+      // 清空 token
+      window.sessionStorage.clear();
+      // 跳转至登录页
+      this.$router.push('/login');
+    }
+  }
+}
+</script>
+
+<style  lang='less' scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,30 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
+  { path:'/home', component: Home}
 ]
 
 const router = new VueRouter({
   routes
+})
+
+// 路由导航守卫控制页面访问权限
+router.beforeEach((to,from,next) => {
+  // 1.如果访问登录页，直接放行
+  if(to.path === '/login') return next();
+  // 2.如果访问的是其他的需要权限的页面，确认是否有 token
+  const tokenStr = window.sessionStorage.getItem('token');
+  //  2.1 如果没有 token ，强制进入登录页
+  if(!tokenStr) return next('/login');
+  //  2.2 有 token ，放行
+  next();
 })
 
 export default router
