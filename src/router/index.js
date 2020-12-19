6cb804d90e773177179230e562cb8d9da87b05e9
hi518 <2362391092@qq.com>
2020-12-19 17:54:28 +0800

    完成报表功能

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,8 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/Params.vue'
 import Goods from '../components/goods/Goods.vue'
 import Add from '../components/goods/Add.vue'
-import Orders from '../components/order/Orders'
+import Orders from '../components/order/Orders.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 
@@ -30,7 +31,8 @@ const routes = [
       { path: '/params', component: Params},
       { path: '/goods', component: Goods },
       { path: '/goods/add', component: Add},
-      { path: '/orders', component: Orders}
+      { path: '/orders', component: Orders},
+      { path: '/reports', component: Report}
     ]
   }
 ]
